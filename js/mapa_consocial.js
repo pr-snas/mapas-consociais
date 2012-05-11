cd9c86152dae3e266b47b247288f76516a614b3c
Marcos Lopes <marcosmlopes01@gmail.com>
2012-05-11 17:56:58 -0300

    Implementando a criação dos marcadores no mapa

--- js/mapa_consocial.js
+++ js/mapa_consocial.js
@@ -8,22 +8,46 @@ $(function() {
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
 
-  window.mapa = new google.maps.Map($('#map_canvas')[0], opcoes);
+  window.map = new google.maps.Map($('#map_canvas')[0], opcoes);
+  window.infoWindow = new google.maps.InfoWindow()
+
+  function attachClickEvent(marker) {
+    google.maps.event.addListener(marker, 'click', function() {
+      window.infoWindow.setContent(marker.content);
+      window.infoWindow.open(window.map, marker);
+    });
+  }
 
   $.getJSON('pontos.json', function(data) {
-    console.log(data);
-    return
-
     if (data) {
-      var i, item, marker;
+      var i, item, marker, latlng, c;
       for (i = 0; i < data.length; i++) {
         item = data[i];
+
+        if (!item.lat || !item.lng) {
+          continue;
+        }
+
+        latlng = new google.maps.LatLng(item.lat, item.lng);
+
         marker = new google.maps.Marker({
-          map: window.mapa,
-          position: false,
-          title: ""
+          map: window.map,
+          position: latlng,
+          title: item.titulo
         });
-        window.markers.push(marker);
+
+        marker.content = "<h1>" + item.titulo + "</h1>";
+        marker.content += "<ul>";
+
+        for (c = 0; c < item.campos.length; c++) {
+          for (var key in item.campos[c]) {
+            marker.content += "<li><strong>" + key + "</strong>: " + item.campos[c][key];
+          }
+        }
+
+        marker.content += "</ul>";
+
+        attachClickEvent(marker);
       }
     }
   });
